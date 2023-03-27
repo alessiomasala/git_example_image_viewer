14f8d2cb14688b378891163a9eea74f43dddc022
alessiomasala <alessio.masala@stud.unifi.it>
2023-03-26 17:28:52 -0700

    Added version name

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by Alessio Masala on 26/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+static const std::string VERSION = "0.1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
