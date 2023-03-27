1281cd8bd182480aee3b30957bfa12a4b79c8f0f
alessiomasala <alessio.masala@stud.unifi.it>
2023-03-26 18:00:21 -0700

    Added file_change

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -7,9 +7,24 @@ BitmapImage::BitmapImage() : width(640), height(480), channel(3), depth(8){
 bool BitmapImage::load(std::string name) {
     // check it is a supported image file
     std::string fileExt = name.substr(name.find_last_of(".") + 1);
-    if (fileExt == "jpg") {
+    if ((fileExt == "jpg") || (fileExt == "png")) {
         // load image file
         // decompress into buffer
+        return true;
+    }
+    return false;
+}
+
+bool BitmapImage::resize(int width, int height) {
+    // resize buffer
+    return true;
+}
+
+bool BitmapImage::save(std::string name) {
+    // check it is a supported image file
+    std::string fileExt = name.substr(name.find_last_of(".") + 1);
+    if (fileExt == "jpg") {
+        // compress buffer into image file
         return true;
     }
     return false;
